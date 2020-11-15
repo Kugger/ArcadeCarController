13b90a741ef7f0c48dc464007aa3d7769888deb5
Kacper Gorgoń <kacper15011@gmail.com>
2020-11-15 21:41:29 +0100

    Update CarControllerTest01 Script

--- Arcade Car Controller V4.0s/Assets/CarControllerTest01.cs
+++ Arcade Car Controller V4.0s/Assets/CarControllerTest01.cs
@@ -4,15 +4,80 @@ using UnityEngine;
 
 public class CarControllerTest01 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform kartModel;
+    public Transform kartNormal;
+    public Rigidbody sphere;
+
+    float speed, currentSpeed;
+    float rotate, currentRotate;
+
+    [Header("Bools")]
+    public bool drifting;
+
+    [Header("Parameters")]
+
+    public float acceleration = 30f;
+    public float steering = 80f;
+    public float gravity = 10f;
+    public LayerMask layerMask;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            float time = Time.timeScale == 1 ? .2f : 1;
+            Time.timeScale = time;
+        }
+
+        //Follow Collider
+        transform.position = sphere.transform.position - new Vector3(0, 0.4f, 0);
+
+        //Accelerate
+        if (Input.GetButton("Fire1"))
+            speed = acceleration;
+
+        //Steer
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            int dir = Input.GetAxis("Horizontal") > 0 ? 1 : -1;
+            float amount = Mathf.Abs((Input.GetAxis("Horizontal")));
+            Steer(dir, amount);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        //Forward Acceleration
+        if (!drifting)
+            sphere.AddForce(-kartModel.transform.right * currentSpeed, ForceMode.Acceleration);
+        else
+            sphere.AddForce(transform.forward * currentSpeed, ForceMode.Acceleration);
+
+        //Gravity
+        sphere.AddForce(Vector3.down * gravity, ForceMode.Acceleration);
+
+        //Steering
+        transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, new Vector3(0, transform.eulerAngles.y + currentRotate, 0), Time.deltaTime * 5f);
+
+        RaycastHit hitOn;
+        RaycastHit hitNear;
+
+        Physics.Raycast(transform.position + (-transform.up * .5f), Vector3.down, out hitOn, 1.1f, layerMask);
+        Physics.Raycast(transform.position + (-transform.up * .1f), Vector3.down, out hitNear, 2.0f, layerMask);
+
+        //Normal Rotation
+        kartNormal.up = Vector3.Lerp(kartNormal.up, hitNear.normal, Time.deltaTime * 8.0f);
+        kartNormal.Rotate(0, transform.eulerAngles.y, 0);
+    }
+
+    public void Steer(int direction, float amount)
+    {
+        rotate = (steering * direction) * amount;
+    }
+
+    private void Speed(float x)
+    {
+        currentSpeed = x;
     }
 }
